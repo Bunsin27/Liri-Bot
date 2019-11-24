3895ed4e1a7247bc4ea1ab89ae54a56fe51ee750
bunsin27 <sengland27@hotmail.com>
2019-11-24 16:44:39 -0500

    i realy dont like this app

---
+++ keys.js
@@ -0,0 +1,6 @@
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID = '7af65728bd254978bb001f8cbb873afd',
+  secret: process.env.SPOTIFY_SECRET = '62da6ef6489c449c9c46dd7c2b4ef2ce'
+};
